6c4aa8042551d52a270400ca9594ccca12f1bd3c
wory04 <voroseszterveronika@gmail.com>
2020-03-03 14:26:07 +0100

    remove useless files and usings

--- HarMoney/Contexts/TransactionContext.cs
+++ HarMoney/Contexts/TransactionContext.cs
@@ -5,7 +5,6 @@ namespace HarMoney.Contexts
 {
     public class TransactionContext : DbContext
     {
-        TransactionContext() { }
         public TransactionContext(DbContextOptions<TransactionContext> options) : base(options) { }
 
         public DbSet<Transaction> Transactions { get; set; }
