e74d713eb0cf1418d829d788025112f78c710e68
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-03-03 23:55:41 +0100

    add entity type configuration for transaction model

---
+++ HarMoney/Contexts/Configurations/TransactionEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HarMoney.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HarMoney.Contexts.Configurations
+{
+    internal class TransactionEntityTypeConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("transaction");
+            builder.HasKey(prop => prop.Id);
+            builder.Property(prop => prop.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            builder.Property(prop => prop.Title).HasColumnName("title").HasMaxLength(30).IsRequired();
+            builder.Property(prop => prop.DueDate).HasColumnName("due_date").IsRequired();
+            builder.Property(prop => prop.Amount).HasColumnName("amount").IsRequired();
+            builder.Property(prop => prop.Frequency).HasColumnName("frequency").IsRequired()
+                .HasConversion(
+                    f => f.ToString(),
+                    f => (Frequency)Enum.Parse(typeof(Frequency), f));
+            builder.Property(prop => prop.Direction).HasColumnName("direction").IsRequired()
+                .HasConversion(
+                    d => d.ToString(),
+                    d => (Direction)Enum.Parse(typeof(Direction), d));
+        }
+    }
+}
