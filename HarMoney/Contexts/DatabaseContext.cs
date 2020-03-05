91dfd211dcb0d22d0ca1f06e91c9ec3bb404081a
Nikolett Szucs <ts2jzgte@gmail.com>
2020-03-05 18:06:20 +0100

    merge data contexts

---
+++ HarMoney/Contexts/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using HarMoney.Contexts.Configurations;
+using HarMoney.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HarMoney.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TransactionEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
+        }
+
+        public DbSet<HarMoney.Models.User> User { get; set; }
+    }
+}
