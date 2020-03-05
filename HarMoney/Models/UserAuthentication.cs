64af6c6450f6e24e18b6c58b1dacea531b3f42e0
Nikolett Szucs <ts2jzgte@gmail.com>
2020-03-05 17:57:11 +0100

    add user authentication model

---
+++ HarMoney/Models/UserAuthentication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarMoney.Models
+{
+    public class UserAuthentication
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
