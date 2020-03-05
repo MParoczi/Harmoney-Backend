95b0eb1751590f9525dcf8e826d2d18335de7455
Nikolett Szucs <ts2jzgte@gmail.com>
2020-03-05 17:59:37 +0100

    add users api endpoint (login/registration)

---
+++ HarMoney/Controllers/UsersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HarMoney.Contexts;
+using HarMoney.Models;
+
+namespace HarMoney.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public UsersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        //[HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        //[HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(long id)
+        {
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        //[HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(long id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost("registration")]
+        public async Task<ActionResult<User>> RegisterUser(User user)
+        {
+            var userByEmail = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            if (userByEmail == null)
+            {
+                _context.User.Add(user);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetUser", new {id = user.Id}, user);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> LoginUser(UserAuthentication user)
+        {
+            var userByEmail = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            if (userByEmail == null || user.Password != userByEmail.Password)
+            {
+                return BadRequest();
+            }
+            return userByEmail;
+        }
+
+        // DELETE: api/Users/5
+        //[HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(long id)
+        {
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private bool UserExists(long id)
+        {
+            return _context.User.Any(e => e.Id == id);
+        }
+    }
+}
