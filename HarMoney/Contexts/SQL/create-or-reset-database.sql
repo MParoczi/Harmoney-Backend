17b55bb5063092b5de2903a2e0103a055ba00dc6
wory04 <voroseszterveronika@gmail.com>
2020-03-05 22:56:17 +0100

    fix sql syntax

--- HarMoney/Contexts/SQL/create-or-reset-database.sql
+++ HarMoney/Contexts/SQL/create-or-reset-database.sql
@@ -10,7 +10,7 @@ CREATE TABLE transaction (
 );
 
 INSERT INTO public.transaction (title, due_date, amount, frequency, direction)
-    VALUES ('zsebpénz', TO_DATE('03/03/2020', 'DD/MM/YYYY'), 20000, 'Single', 'Income');
+    VALUES ('zsebpénz', TO_DATE('03/03/2020', 'DD/MM/YYYY'), 20000, 'Single', 'Income'),
            ('gázszámla', TO_DATE('15/03/2020', 'DD/MM/YYYY'), 13458, 'Monthly', 'Expenditure'),
            ('fizetés', TO_DATE('01/03/2020', 'DD/MM/YYYY'), 300000, 'Monthly', 'Income'),
            ('közös költség', TO_DATE('09/03/2020', 'DD/MM/YYYY'), 28236, 'Monthly', 'Expenditure');
