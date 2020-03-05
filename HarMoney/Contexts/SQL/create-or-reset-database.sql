49f081e05f48517d98074f0cd1b62cf54b7df8ce
Nikolett Szucs <ts2jzgte@gmail.com>
2020-03-05 18:11:50 +0100

    add user table creation

--- HarMoney/Contexts/SQL/create-or-reset-database.sql
+++ HarMoney/Contexts/SQL/create-or-reset-database.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS public.transaction;
 DROP SEQUENCE IF EXISTS public.transaction_id_seq;
+DROP TABLE IF EXISTS public.user_data;
+DROP SEQUENCE IF EXISTS public.user_data_id_seq;
+
 CREATE TABLE transaction (
     id serial PRIMARY KEY,
     title VARCHAR(30),
@@ -11,3 +14,15 @@ CREATE TABLE transaction (
 
 INSERT INTO public.transaction (title, due_date, amount, frequency, direction)
     VALUES ('zsebpénz', TO_DATE('03/03/2020', 'DD/MM/YYYY'), 20000, 'Single', 'Income');
+
+CREATE TABLE user_data (
+    id serial PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    password VARCHAR (20) NOT NULL,
+    token TEXT
+);
+
+INSERT INTO public.user_data (first_name, last_name, email, password)
+    VALUES ('Kutya', 'Füle', 'a@b.hu', 'admin')
