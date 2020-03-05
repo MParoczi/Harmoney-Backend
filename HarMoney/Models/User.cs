5f84150349d3becfaae4e6207feeceab2b3a2392
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-03-05 20:22:30 +0100

    create user dto

--- HarMoney/Models/User.cs
+++ HarMoney/Models/User.cs
@@ -14,4 +14,20 @@ namespace HarMoney.Models
         public string Password { get; set; }
         public string Token { get; set; }
     }
+
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public UserDTO(User user)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Email = user.Email;
+        }
+    }
 }
