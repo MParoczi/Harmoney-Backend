e4b843a7dd17fb90b7947d6121ea582164ce4cf9
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-31 19:19:16 +0200

    Add Logger to AccountController

--- HarMoney/Controllers/AccountController.cs
+++ HarMoney/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Composition;
 using System.Threading.Tasks;
 using HarMoney.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace HarMoney.Controllers
 {
@@ -12,11 +14,13 @@ namespace HarMoney.Controllers
     {
         private UserManager<User> UserManager { get; }
         private SignInManager<User> SignInManager  { get; }
+        private ILogger<AccountController> Logger  { get; }
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<AccountController> logger)
         {
             UserManager = userManager;
             SignInManager = signInManager;
+            Logger = logger;
         }
 
         public async Task<ActionResult<UserDto>> Register([FromBody] UserRegistration model)
