e355d4ce93393c2e17a9a36e9f870827607fb2b6
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-31 22:06:39 +0200

    Add EmailSender class

---
+++ HarMoney/EmailService/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿namespace EmailService
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfig;
+ 
+        public EmailSender(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+ 
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+ 
+            Send(emailMessage);
+        }
+    }
+}
