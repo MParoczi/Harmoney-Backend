e5cbab1f93d7e5f0d2a69896552d9df92027f6ac
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-04-01 22:32:54 +0200

    Add jwt token auth feature to controller

--- HarMoney/Controllers/AccountController.cs
+++ HarMoney/Controllers/AccountController.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Composition;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using HarMoney.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HarMoney.Controllers
 {
     [Route("api/[controller]/[action]")]
     public class AccountController : Controller
     {
+        private const string SECRET_KEY = "harmoney_secret_key";
+        public static readonly SymmetricSecurityKey SIGNIN_KEY = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SECRET_KEY));
+
         private UserManager<User> UserManager { get; }
         private SignInManager<User> SignInManager  { get; }
 
@@ -41,15 +50,20 @@ namespace HarMoney.Controllers
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
             if (result.Succeeded)
             {
-                return new UserDto(user);
+                var token = GenerateToken(model.Email, user.FirstName, user.LastName, user.Id.ToString());
+                return new UserDto()
+                {
+                    FirstName = user.LastName,
+                    LastName = user.FirstName,
+                    Email = user.Email,
+                    Token = token
+                };
             }
-            else
-            {
-                return BadRequest();
-            }
+
+            return BadRequest();
         }
-        
-        public async Task<int> Logout([FromBody]UserDto userToLogout)
+        [HttpPost]
+        public async Task<int> Logout([FromBody]User userToLogout)
         {
             var user = await UserManager.FindByEmailAsync(userToLogout.Email);
             await UserManager.UpdateSecurityStampAsync(user);
@@ -57,6 +71,33 @@ namespace HarMoney.Controllers
             await SignInManager.SignOutAsync();
             return 204;
         }
-            
+
+        [HttpPost]
+        public async Task<UserDto> CurrentUser([FromBody]Token jwt)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var result = jwtHandler.ReadToken(jwt.TokenString) as JwtSecurityToken;
+            var Email = result.Claims.First(claim => claim.Type == ClaimTypes.UserData).Value;
+            var user = await UserManager.FindByEmailAsync(Email);
+            return new UserDto(user);
+        }
+        
+
+        private string GenerateToken(string username, string firstName, string lastName, string id)
+        {
+            var token = new JwtSecurityToken(
+                claims: new Claim[]
+                {
+                    new Claim(ClaimTypes.UserData, username), 
+                    new Claim(ClaimTypes.Name, id), 
+                    new Claim(ClaimTypes.Name, lastName), 
+                    new Claim(ClaimTypes.Name, firstName), 
+                },
+                notBefore: new DateTimeOffset(DateTime.Now).DateTime,
+                expires: new DateTimeOffset(DateTime.Now.AddMinutes(15)).DateTime,
+                signingCredentials: new SigningCredentials(SIGNIN_KEY, SecurityAlgorithms.HmacSha256)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
