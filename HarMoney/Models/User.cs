b6dc3ae8833c43cfbfd4580bb4d2927745bf85c6
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-04-01 22:33:26 +0200

    Add token prop

--- HarMoney/Models/User.cs
+++ HarMoney/Models/User.cs
@@ -15,6 +15,7 @@ namespace HarMoney.Models
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Token { get; set; }
 
         public UserDto()
         {
